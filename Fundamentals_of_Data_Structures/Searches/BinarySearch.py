c6364abbd2b64362cf2cd48a49dff4f97d91f683
AntoineGaton <AntoineGaton@gmail.com>
2023-11-19 21:00:52 -0500

    updated binary search

--- Fundamentals_of_Data_Structures/Searches/BinarySearch.py
+++ Fundamentals_of_Data_Structures/Searches/BinarySearch.py
@@ -1,41 +1,41 @@
-def BinarySearch(numbers, numbersSize, key):
-   mid = 0
+def binary_search(numbers, key):
+   # Variables to hold the low, middle and high indices
+   # of the area being searched. Starts with entire range.
    low = 0
-   high = numbersSize - 1
-   
+   mid = len(numbers) // 2
+   high = len(numbers) - 1
+
+   # Loop until "low" passes "high"
    while (high >= low):
-      mid = (high + low) / 2
+      # calculate the middle index
+      mid = (high + low) // 2
+
+      # Cut the range to either the left or right half,
+      # unless numbers[mid] is the key
       if (numbers[mid] < key):
          low = mid + 1
+   
       elif (numbers[mid] > key):
          high = mid - 1
+   
       else:
-         return mid
+         return mid   
+
    return -1 # not found
 
 def main():
-   numbers = { 2, 4, 7, 10, 11, 32, 45, 87 }
-   NUMBERS_SIZE = 8
-   i = 0
-   key = 0
-   keyIndex = 0
-   
-   print("NUMBERS: ")
-   
-   for i in range(NUMBERS_SIZE):
-      print(numbers[i] + " ")
+   # Main program to test the binary_search()function   
+   numbers = [2, 4, 7, 10, 11, 32, 45, 87]
+   print('NUMBERS:', numbers)
       
-   print()
-   
-   print("Enter a value: ")
-   key = int(input())
-   
-   keyIndex = BinarySearch(numbers, NUMBERS_SIZE, key)
-   
-   if (keyIndex == -1):
-      print(key + " was not found.")
+   key = int(input('Enter an integer value: '))
+   key_index = binary_search(numbers, key)
+      
+   if (key_index == -1):
+      print('%d was not found.' % key)
    else:
-      print("Found " + key + " at index " + keyIndex + ".")
-
+      print('Found %d at index %d.' % (key, key_index))
+      return 0
+      
 if __name__ == '__main__':
    main()
