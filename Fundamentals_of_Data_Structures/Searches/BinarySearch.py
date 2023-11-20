bccc8f657cd51837cc42c89ff08bccb1b1abe09c
AntoineGaton <AntoineGaton@gmail.com>
2023-11-19 21:09:29 -0500

    adding recursion to binary search

--- Fundamentals_of_Data_Structures/Searches/BinarySearch.py
+++ Fundamentals_of_Data_Structures/Searches/BinarySearch.py
@@ -39,3 +39,20 @@ def main():
       
 if __name__ == '__main__':
    main()
+   
+'''
+Binary Search with recursion
+BinarySearch(numbers, low, high, key) {
+   if (low > high)
+      return -1
+
+   mid = (low + high) / 2
+   if (numbers[mid] < key) {
+      return BinarySearch(numbers, mid + 1, high, key)
+   }
+   else if (numbers[mid] > key) {
+      return BinarySearch(numbers, low, mid - 1, key)
+   }
+   return mid
+}
+'''
